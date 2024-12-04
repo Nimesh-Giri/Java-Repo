cda85e6bbb54d5034d120fe5e44a06085c4eb62b
Nimesh-Giri <nimeshgiri871@gmail.com>
2024-12-04 16:36:48 +0545

    once again trying

--- pracitcal1/Areaoftriangle.java
+++ pracitcal1/Areaoftriangle.java
@@ -4,12 +4,12 @@ import java.util.*;
 public class Areaoftriangle {
  public static void main(String[] args){
  Scanner sc= new Scanner(System.in);
-System.out.println("Enter a ,b,c for the triangle");
+System.out.println("Enter a ,b,d for the triangle");
 double a =sc.nextDouble();
 double b= sc.nextDouble();
-double c= sc.nextDouble();
-double s= (a+b+c)/2;
-double area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+double d= sc.nextDouble();
+double s= (a+b+d)/2;
+double area = Math.sqrt(s*(s-a)*(s-b)*(s-d));
 sc.close();
 System.out.println("the area of triangle:"+ area);
 }
